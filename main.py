18e3a6c69dfb4d800d66aac7d9f2cdccd9ec2b9c
Juan David <juandavidgodoy@hotmail.com>
2024-10-24 23:01:05 -0500

    Actualice la forma de leer el codigo

--- main.py
+++ main.py
@@ -3,25 +3,42 @@ from mips.lexer import tokenize
 from mips.parser import Parser
 from mips.interpreter import Interpreter
 
+
 def main():
-    code = input()  
+    def leer_archivo_txt(ruta_archivo):
+        try:
+            with open(ruta_archivo, 'r', encoding='utf-8') as archivo:
+                contenido = archivo.read()
+                return contenido
+        except FileNotFoundError:
+            print(f"El archivo {ruta_archivo} no se encontró.")
+        except Exception as e:
+            print(f"Ocurrió un error al leer el archivo: {e}")
 
-    tokens_list = tokenize(code)
-    print("Tokens:", tokens_list)
+    ruta = './comando.txt'
+    contenido = leer_archivo_txt(ruta)
+    if contenido:
+        pipeline = contenido.split('\n')
+        print(f"Código a interpretar: {pipeline}")
+        for code in pipeline:
 
-    parser_instance = Parser(tokens_list)
-    ast = parser_instance.parse_operation()
-    print("AST:", ast)
+            tokens_list = tokenize(code)
+            print("Tokens:", tokens_list)
 
-    interpreter_instance = Interpreter()
-    result = interpreter_instance.visit(ast)
-    print("Resultado de la interpretación:", result)
+            parser_instance = Parser(tokens_list)
+            ast = parser_instance.parse_operation()
+            print("AST:", ast)
 
-    codegen = MIPSCodeGenerator()
-    codegen.generate(ast)
-    mips_code = codegen.get_code()
-    print("Código MIPS generado:")
-    print(mips_code)
+            interpreter_instance = Interpreter()
+            result = interpreter_instance.visit(ast)
+            print("Resultado de la interpretación:", result)
+
+            codegen = MIPSCodeGenerator()
+            codegen.generate(ast)
+            mips_code = codegen.get_code()
+            print("Código MIPS generado:")
+            print(mips_code)
+
 
 if __name__ == '__main__':
     main()
